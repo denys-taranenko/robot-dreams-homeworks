d4e1784f5d419f7677f20f6a9a437ede81f176d4
denys-taranenko <sotar.channel@gmail.com>
2023-07-04 13:08:32 +0300

    Cast input to month in task 5

--- robot-dreams-homework-5/robot-dreams-homework-5.cpp
+++ robot-dreams-homework-5/robot-dreams-homework-5.cpp
@@ -2,7 +2,7 @@
 #include <iomanip>
 
 enum class Month {
-    January,
+    January = 1,
     February,
     March,
     April,
@@ -126,56 +126,44 @@ int main()
     int input{};
     std::cin >> input;
 
-    Month month = Month::January;
+    Month month = Month(input);
 
-    switch (input)
+    switch (month)
     {
-    case 1:
-        month = Month::January;
+    case Month::January:
         std::cout << "January" << std::endl;
         break;
-    case 2:
-        month = Month::February;
+    case Month::February:
         std::cout << "February" << std::endl;
         break;
-    case 3:
-        month = Month::March;
+    case Month::March:
         std::cout << "March" << std::endl;
         break;
-    case 4:
-        month = Month::April;
+    case Month::April:
         std::cout << "April" << std::endl;
         break;
-    case 5:
-        month = Month::May;
+    case Month::May:
         std::cout << "May" << std::endl;
         break;
-    case 6:
-        month = Month::June;
+    case Month::June:
         std::cout << "June" << std::endl;
         break;
-    case 7:
-        month = Month::July;
+    case Month::July:
         std::cout << "July" << std::endl;
         break;
-    case 8:
-        month = Month::August;
+    case Month::August:
         std::cout << "August" << std::endl;
         break;
-    case 9:
-        month = Month::September;
+    case Month::September:
         std::cout << "September" << std::endl;
         break;
-    case 10:
-        month = Month::October;
+    case Month::October:
         std::cout << "October" << std::endl;
         break;
-    case 11:
-        month = Month::November;
+    case Month::November:
         std::cout << "November" << std::endl;
         break;
-    case 12:
-        month = Month::December;
+    case Month::December:
         std::cout << "December" << std::endl;
         break;
     default:
